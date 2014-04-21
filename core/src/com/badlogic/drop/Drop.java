99416c3b349af2e5689c3b8de622e6327433a01f
Kevin London <kevinlondon@gmail.com>
2014-04-20 23:00:25 -0700

    End of first part of the demo. Add methods to draw and update raindrops and dispose of native images when done.

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -1,5 +1,7 @@
 package com.badlogic.drop;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
@@ -9,20 +11,23 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Drop extends ApplicationAdapter {
 	Texture dropImage;
 	Texture bucketImage;
 	Sound dropSound;
 	Music rainMusic;
-	
 	OrthographicCamera camera;
 	SpriteBatch batch;
-	
 	Rectangle bucket;
 	Vector3 touchPos;
+	Array<Rectangle> raindrops;
+	long lastDropTime;
 	
 	@Override
 	public void create () {
@@ -52,28 +57,75 @@ public class Drop extends ApplicationAdapter {
 		bucket.y = 20;
 		bucket.height = 64;
 		
+		// Create Raindrops and spawn the first one.
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
 	}
 
 	@Override
 	public void render () {
+		/* Clear screen with a dark blue color.
+		 * Arguments to ClearColor are r g b, alpha
+		 */
 		Gdx.gl.glClearColor(0, 0, .2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		// Draw the bucket and all the drops.
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for (Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
 		
+		// Process any user input
 		if (Gdx.input.isTouched()) {
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
 			bucket.x = touchPos.x - bucket.width / 2;
 		}
 		
+		// Ensure that the bucket's within the screen bounds
 		if (Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
 		if (Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
 		if (bucket.x < 0) bucket.x = 0;
 		if (bucket.x > 800 - bucket.width) bucket.x = 800 - bucket.width;
+		
+		// Check time since last raindrop. Do we need another?
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+		
+		// Update all the raindrops
+		Iterator<Rectangle> iter = raindrops.iterator();
+		while (iter.hasNext()) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (raindrop.y + raindrop.height < 0) iter.remove();
+			if (raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
+	}
+	
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+	
+	@Override
+	public void dispose() {
+		// Clear all the "native" resources
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+		batch.dispose();
 	}
 }
